dc3bf8b9288bd9fb7b13ed5d06108d69f0c3a39a
Sabina <dzucevasabina@gmail.com>
2021-11-08 12:56:18 +0300

    Make service load contacts from contact provider

---
+++ MyApp/app/src/main/java/com/gmail/dzutsevasabina/interfaces/ContactClickListener.kt
@@ -0,0 +1,7 @@
+package com.gmail.dzutsevasabina.interfaces
+
+import android.view.View
+
+interface ContactClickListener {
+    fun onClick(view: View?, id: String)
+}
--- MyApp/app/src/main/java/com/gmail/dzutsevasabina/model/Contact.kt
+++ MyApp/app/src/main/java/com/gmail/dzutsevasabina/model/Contact.kt
@@ -1,13 +1,21 @@
 package com.gmail.dzutsevasabina.model
 
-data class Contact(
-    val image: Int,
+data class BriefContact(
+    val id: String,
+    val image: String,
     val name: String,
-    val birthday: String,
+    val phoneNumber1: String,
+)
+
+data class DetailedContact(
+    val id: String,
+    val image: String,
+    val name: String,
     val phoneNumber1: String,
     val phoneNumber2: String,
     val email1: String,
     val email2: String,
+    val birthday: String,
     val description: String,
     var sendBirthdayNotifications: Boolean = false
 )
